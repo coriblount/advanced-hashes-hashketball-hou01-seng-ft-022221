497bdf8d728f9f13ecd5380b205ed5832371e0e2
cori <heycorihey@icloud.com>
2021-02-14 18:55:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,20 +180,18 @@ def num_points_scored(player_search)
    return output
  end
  
- def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # 
-            return player
-          end
-        end
-      end
-    end
+ def player_stats(player_name, hashketball)
+  player_name.capitalize!
+  if hashketball[:home][:players].include?(player_name)
+    hashketball[:home][:players][player_name][:stats]
+  elsif hashketball[:away][:players].include?(player_name)
+    hashketball[:away][:players][player_name][:stats]
+  else
+    "No player found."
   end
 end
+
+puts player_stats("Daffy", hashketball)
  
  def big_shoe_rebounds
   big_shoe = 0
