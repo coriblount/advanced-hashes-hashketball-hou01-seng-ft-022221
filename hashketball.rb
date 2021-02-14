a7c750c60fdf99192f15063f382d4e2704ac348d
cori <heycorihey@icloud.com>
2021-02-14 19:01:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,7 +180,7 @@ def num_points_scored(player_search)
    return output
  end
  
- def player_stats
+ def player_stats(player_name)
    binding.pry
  game_hash.each do |team, team_info|
     team_info.each do |key, value|
